ccaff29d14c700dfd7afbe0179e68a4b03fa6bfe
Ahmed Saad <ask@shopbox.com>
2018-09-13 01:34:28 +0200

    SearchingRepositoryInterface

---
+++ app/modules/api/repositories/SearchingRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* interface for git searching repository
+*/
+namespace app\modules\api\repositories;
+
+use app\modules\api\adapters\SearchingAdapterInterface;
+
+interface SearchingRepositoryInterface
+{	
+	public function setsearchingAdapter(SearchingAdapterInterface $searchingAdapter);
+
+    public function search(String $query);
+}
