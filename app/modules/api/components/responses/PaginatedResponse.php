2755a1dcbbaed3e26d144780baec8316dc8ba512
Ahmed Saad <ask@shopbox.com>
2018-09-13 23:57:22 +0200

    PaginatedResponse

--- app/modules/api/components/responses/PaginatedResponse.php
+++ app/modules/api/components/responses/PaginatedResponse.php
@@ -1,11 +1,11 @@
 <?php
+/**
+ * return the paginated data from the data providor with the pagination meta data
+ * */
 namespace app\modules\api\components\responses;
 
 class PaginatedResponse implements ResponseInterface
 {
-	/**
-	 * return the paginated data from the data providor with the pagination meta data
-	 * */
 	public function getResponse($dataProvidor)
 	{
 		return [
