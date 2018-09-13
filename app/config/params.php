a286ee7e5e7268d7fe8d701e490f887af45743a1
Ahmed Saad <ask@shopbox.com>
2018-09-14 00:07:02 +0200

    add new github access token param

--- app/config/params.php
+++ app/config/params.php
@@ -2,5 +2,5 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'github_access_token' => 'a70b53ec2f2aa44df6179f74c29b382190c7eaeb',
+    'github_access_token' => '5638cc6eb82dcf4c0da3fdb84cf3ff09c6306211',
 ];
