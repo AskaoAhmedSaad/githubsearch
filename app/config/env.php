e84c3244268f802ca531aa5381986338a2bf9160
Ahmed Saad <ask@shopbox.com>
2018-09-23 10:19:24 +0200

    using enviroment variables

---
+++ app/config/env.php
@@ -0,0 +1,3 @@
+<?php
+
+defined('GITHUB_ACCESS_TOKEN') or define('GITHUB_ACCESS_TOKEN', '{{set you access token here from your github account}}');
--- app/config/params.php
+++ app/config/params.php
@@ -2,5 +2,4 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'github_access_token' => '{{set you access token here from your github account}}',
 ];
